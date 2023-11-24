cd55be2b0589b77d423c73a5d586b9155e95e12a
MarvelD27 <145282896+MarvelD27@users.noreply.github.com>
2023-11-24 08:52:23 -0800

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* factorial(int* n);
+
+int main(){
+
+int n;
+
+do{
+printf("Enter a number: ");
+
+scanf("%d" , &n);
+} while(n < 0);
+
+int* f = malloc(sizeof(int));
+
+*f = n;
+
+int* result = factorial(&n);
+
+printf("The factorial of %d is %d" , *f , *result);
+
+}
+
+int* factorial(int* n){
+
+    int* solution = malloc(sizeof(int));
+
+if(*n == 1 || *n == 0){
+   
+   *solution = 1;
+    return solution;
+}
+else{
+
+int temp = *n - 1;
+
+factorial(&temp);
+
+  *n *= temp  ;
+
+return n;
+
+}
+
+}
+
