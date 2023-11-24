85cef2f7dea7043837747e09b5823cd3f0383dd0
MarvelD27 <145282896+MarvelD27@users.noreply.github.com>
2023-11-24 10:51:59 -0800

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* factorial(int* n);
+int *factorial(int* n);
 
 int main(){
 
@@ -21,28 +21,39 @@ int* result = factorial(&n);
 
 printf("The factorial of %d is %d" , *f , *result);
 
+free(f);
+
+return 0;
+
 }
 
-int* factorial(int* n){
+int *factorial(int* n){
 
-    int* solution = malloc(sizeof(int));
+int* solution = malloc(sizeof(int));
 
-if(*n == 1 || *n == 0){
+    if(*n == 1 || *n == 0){
    
    *solution = 1;
     return solution;
-}
-else{
 
-int temp = *n - 1;
+   }
 
-factorial(&temp);
+   else{
 
-  *n *= temp  ;
+   int* temp = malloc(sizeof(int));
 
-return n;
+   *temp = *n - 1;
 
-}
+   factorial(temp);
+
+   *n *= *temp  ;
+
+   free(temp);
+
+   return n;
+
+
+   }
 
 }
 
